b78c60a5ac9d7ade421610b908f8f3de29945765
NGALA MAC <macngala4@gmail.com>
2023-10-17 18:53:01 +0100

    Fix rubocop linter errors

--- tdd/solver.rb
+++ tdd/solver.rb
@@ -2,9 +2,9 @@ class Solver
   def self.factorial(int)
     if int.negative?
       raise ArgumentError,
-            'Factorial is not defined for negaive numbers, please imput numbers from 0 and above'
+            'Factorial is not defined for negative numbers. Please input non-negative integers (0 and above).'
     end
-    return 1 if intr <= 1
+    return 1 if int <= 1
 
     (1..int).reduce(:*)
   end
