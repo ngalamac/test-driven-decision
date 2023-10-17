1c6796730fae27ff0bbb5398389c1a281141338b
NGALA MAC <macngala4@gmail.com>
2023-10-17 18:20:34 +0100

    Add Solver class

---
+++ tdd/solver.rb
@@ -0,0 +1,16 @@
+class Solver
+  def self.factorial(int)
+    if int.negative?
+      raise ArgumentError,
+            'Factorial is not defined for negaive numbers, please imput numbers from 0 and above'
+    end
+    return 1 if intr <= 1
+
+    (1..int).reduce(:*)
+  end
+
+  def self.reverse(word)
+    word.reverse
+  end
+
+end
